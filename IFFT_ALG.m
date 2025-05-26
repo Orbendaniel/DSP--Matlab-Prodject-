adb4614c735e847466def5da413f3f984d3d863e
Orbendaniel <89739774+Orbendaniel@users.noreply.github.com>
2025-05-26 17:13:57 +0300

    הוספתי את החרא שעידו רצה לעשות בהערה

--- IFFT_ALG.m
+++ IFFT_ALG.m
@@ -2,4 +2,7 @@ function [fout] = IFFT_ALG(fin)
     % assume length(F)=2^n
     N = length(fin);
     fout = conj( FFT_ALG(conj(fin)) )/N;
+    %N = length(fin);
+    %y =FFT_ALG(fin) /N;
+    %fout = [y(1), y(end:-1:2)];
 end
