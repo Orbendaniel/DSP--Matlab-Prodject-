619a746e633e3a796e20b538b1a95dae103f4439
alonk126 <alonk2710@gmail.com>
2025-06-01 15:01:39 +0300

    תרגיל ח

--- FFT2_ALG.m
+++ FFT2_ALG.m
@@ -4,7 +4,7 @@ function X = FFT2_ALG(x)
     
     %Loop over each row
     for r = 1:N                   
-        X(r, :)= FFT_ALG(x(r, :));  
+        X(r, :)= fft(x(r, :));  
         % x(r, :) selects entire r-th row as a 1×M vector
         % FFT_ALG(… ) returns its 1-D DFT (also 1×M)
         % we store that back into row r of X
@@ -13,7 +13,7 @@ function X = FFT2_ALG(x)
     % 4) Loop over each column
     for c = 1:M                   
         col= X(:, c).';   % X(:,c) grabs column c as N×1, then .' transposes to 1×N
-        fft_row= FFT_ALG(col);
+        fft_row= fft(col);
         % now fft_row is the 1×N DFT of that column
         X(:, c)= fft_row.';   % transpose back to N×1 and store into column c of X
     end                           
---
+++ test2.m
@@ -0,0 +1,3 @@
+x = [1,2;
+    3,4];
+disp(IFFT2_ALG(FFT2_ALG(x)))
